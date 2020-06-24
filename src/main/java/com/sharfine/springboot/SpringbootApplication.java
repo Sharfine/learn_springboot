229ee82c5d5a35d8224b961be7421fc8c36c4765
Sharfine <784751530@qq.com>
2020-06-24 11:02:21 +0800

    :sparkles: add AsyncTask

--- src/main/java/com/sharfine/springboot/SpringbootApplication.java
+++ src/main/java/com/sharfine/springboot/SpringbootApplication.java
@@ -3,8 +3,9 @@ package com.sharfine.springboot;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @MapperScan("com.sharfine.springboot.dao")
 @SpringBootApplication
 public class SpringbootApplication {
